20e6d3f93121aa885b0bc665d5196f3a865cee01
David Celis <me@davidcel.is>
2014-03-02 19:39:23 -0800

    Rework Creperie::Loader
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/creperie/loader.rb
+++ lib/creperie/loader.rb
@@ -2,24 +2,46 @@ require 'pathname'
 
 module Creperie
   module Loader
-    EXECUTABLE = 'bin/crepe'
-    def self.crepe_app?
-      original_dir = Dir.pwd
+    class << self
+      # If we find a Gemfile using the method below and it contains Crepe
+      # as a dependency, we can safely assume we're in a Crepe application.
+      def crepe_app?
+        gemfile =~ /gem (['"])crepe\1/
+      end
 
-      loop do
-        # To detect whether or not we're in a Crepe application, we search
-        # recursively through parent directories for a Gemfile that declares
-        # a dependency on Crepe.
-        if File.file?('config.ru')
-          return true if File.read('Gemfile') =~ /gem (['"])crepe\1/
+      def application
+        return unless crepe_app?
+        fild_file('./config/application.rb')
+      end
+
+      # Beginning with the current working directory, recursively search
+      # up through the filesystem for a Gemfile.
+      def gemfile
+        find_file('Gemfile')
+      end
+
+      # Beginning with the current working directory, recursively search
+      # up through the filesystem for a Gemfile.lock.
+      def gemfile_lock
+        find_file('Gemfile.lock')
+      end
+
+      private
+
+      def find_file(filename)
+        original_dir = Dir.pwd
+
+        loop do
+          # For convenience, return the contents of the file if we find it.
+          return File.read(filename) if File.file?(filename)
+
+          # If we've reached the root of the filesystem without finding the
+          # specified file, give up.
+          Dir.chdir(original_dir) and return if Pathname.new(Dir.pwd).root?
+
+          # Otherwise, move up a directory and search again.
+          Dir.chdir('..')
         end
-
-        # If we've reached the root of the filesystem without finding a
-        # Gemfile with Crepe, give up and only provide the `crepe new` command.
-        Dir.chdir(original_dir) and return if Pathname.new(Dir.pwd).root?
-
-        # Otherwise, move up a directory and search again.
-        Dir.chdir('..')
       end
     end
   end
