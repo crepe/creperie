18203e612c7ea2842372c299b2d7c9a6262754b6
David Celis <me@davidcel.is>
2014-03-10 18:49:08 -0700

    Undefine `use` and `run` after loading config.ru
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/creperie/commands/console.rb
+++ lib/creperie/commands/console.rb
@@ -4,22 +4,14 @@ require 'creperie/loader'
 module Creperie
   module Commands
     class Console < Base
-      option ['-E', '--env'], 'ENV', 'Specify the Crêpe environment', default: 'development'
+      option ['-E', '--env'], 'ENV', 'Specify the Crêpe environment', default: 'development' do |env|
+        ENV['CREPE_ENV'] = env
+      end
 
       def execute
-        # The best way to load a Crêpe application's environment is to
-        # find the project's config.ru file and load it, since a rackup file is
-        # required to exist and should load/require all necessary files to run
-        # the application. However, this means we must override Rack::Builder's
-        # DSL methods, namely `use` and `run`. Because these get mixed into
-        # Object, we must override those directly.
-        Object.class_eval do
-          def use(*args) end
-          def run(*args) end
+        with_rack_aliased! do
+          load Loader.config_ru
         end
-
-        ENV['CREPE_ENV'] = env
-        load Loader.config_ru
 
         begin
           require 'pry'
@@ -34,6 +26,32 @@ module Creperie
       def pry?
         gemfile = Loader.gemfile
         File.read(gemfile) =~ /gem (['"])pry/
+      end
+
+      # The best way to load a Crêpe application's environment into an IRB shell
+      # is to load the config.ru file, as that must load/require all code
+      # necessary to run the application. However, this means we must provide
+      # two DSL methods expected to be mixed into Object by Rack::Builder, `use`
+      # and `run`. To prevent the permanence from this, we load config.ru with
+      # those methods defined as no-ops and then undefine them afterwards.
+      def with_rack_aliased!(&block)
+        alias_rack!
+        yield
+        unalias_rack!
+      end
+
+      def alias_rack!
+        Object.class_eval do
+          def use(*args) end
+          def run(*args) end
+        end
+      end
+
+      def unalias_rack!
+        Object.class_eval do
+          undef :use
+          undef :run
+        end
       end
     end
   end
