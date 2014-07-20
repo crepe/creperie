3b482f944985a38c5e39f713d1a6bbe294bba3d1
David Celis <me@davidcel.is>
2014-07-20 11:11:31 -0700

    More template app re-organization
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/creperie/generators/templates/app/app/apis/base_api.rb
+++ lib/creperie/generators/templates/app/app/apis/base_api.rb
@@ -3,10 +3,20 @@
 # entire Crepe application.
 
 class BaseAPI < Crepe::API
+  # Return ActiveRecord connections to the pool when requests terminate.
+  use ActiveRecord::ConnectionAdapters::ConnectionManagement
+
   # Catch ActiveRecord::RecordNotFound exceptions with a 404 response.
-  rescue_from(ActiveRecord::RecordNotFound) { error! :not_found }
+  rescue_from(ActiveRecord::RecordNotFound) do |e|
+    error! :not_found, e.message
+  end
+
+  # Catch ActiveRecord::RecordInvalid exceptions with a 422 response.
+  rescue_from(ActiveRecord::RecordInvalid) do |e|
+    error! :unprocessable_entity, e.message, errors: e.record.errors
+  end
 
   # Add shared helper methods to ApplicationHelper, or create your own
-  # additional helper classes and include them here.
+  # additional application-wide helper modules and include them here.
   helper ApplicationHelper
 end
