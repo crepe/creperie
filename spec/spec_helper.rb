181d5768e695621358d64f2331728050a34a0506
David Celis <me@davidcel.is>
2015-09-23 10:17:17 -0700

    Fix the build
    Signed-off-by: David Celis <me@davidcel.is>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+ENV['CREPE_ENV'] = ENV['RACK_ENV'] = nil
+
 RSpec.configure do |config|
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
